3edca051b13b4f991fc292802fd821b733343146
Joel Sutter <joel-sutter@bluewin.ch>
2011-02-18 13:54:14 +0100

    call libqrgen from python

--- src/pyqrgen/pyqrgen.c
+++ src/pyqrgen/pyqrgen.c
@@ -3,47 +3,62 @@
 
 #include <Python.h>
 
+#include "qrgen.h"
 
 #include <stdio.h>
+#include <string.h>
 
 static Pycairo_CAPI_t *Pycairo_CAPI;
-
-
-
 
 static PyObject* pyqrgen_generate(PyObject *self, PyObject *args)
 {
 	
 	PycairoContext* pycr;
-	if(PyArg_ParseTuple(args, "O!", Pycairo_CAPI->Context_Type, &pycr)) {
+	const char* data;
+	int version;
+	int encMode;
+	int ecLevel;
+	int pixSize;
+	int dataLen;
+	
+	if(PyArg_ParseTuple(args, "s|i|i|i|O!|i", &data, &version, &encMode, &ecLevel, Pycairo_CAPI->Context_Type, &pycr, &pixSize)) {
+		
+		Py_XINCREF(data); 
 		Py_XINCREF(pycr); 
-		printf("ok\n");
+		
+		printf("data: [%s]\n", data);
+		
+		printf("version: %d\n", version);
+		
+		printf("encMode: %d\n", encMode);
+		printf("pixSize: %d\n", pixSize);
+		printf("ecLevel: %d\n", ecLevel);
+		
+		dataLen = strlen(data);
+		
+		printf("dataLen: %d\n", dataLen);
+
 	} else {
 		printf("not ok\n");
 		return NULL;
 	}
 	
+	
+	
 	cairo_t* cr = PycairoContext_GET(pycr);
 	if (!cr) { printf ("cr is NULL\n"); }
+	
+	
+	bool ret = qrgen_generate(data, dataLen, version, encMode, ecLevel, cr, pixSize);
 
-
-
-//	cairo_scale(cr, 100, 100);
-//	cairo_rectangle(cr, 0, 0, 0.5, 0.7);
-	cairo_rectangle(cr, 0, 0, 0.5, 0.7);
-	cairo_set_source_rgba(cr, 0, 1, 1, 0.5);
-	cairo_fill(cr);
-	cairo_surface_t* surface = cairo_get_target(cr);
-	if (surface) {
+	if (ret) {
 	printf ("yes\n");
 	} else {
 	printf ("no\n");
 	}
 
-
-
-
 	Py_DECREF(pycr);
+	Py_DECREF(data); 
 
 	Py_INCREF(Py_None);
 	return Py_None;
--- src/pyqrgen/test.py
+++ src/pyqrgen/test.py
@@ -1,24 +1,24 @@
 #!/usr/bin/env python
 
-import math
 import cairo
 
 import pyqrgen
 
-WIDTH, HEIGHT = 200, 200
+SIZE = 200
 
-surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, WIDTH, HEIGHT)
+surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, SIZE, SIZE)
 
 ctx = cairo.Context(surface)
 
-ctx.scale(WIDTH, HEIGHT)
+#ctx.scale(WIDTH, HEIGHT)
 
-ctx.rectangle(0, 0, 1, 1)
+ctx.rectangle(0, 0, SIZE, SIZE)
 
-ctx.set_source_rgb(0.7, 0.5, 0)
+ctx.set_source_rgb(1, 1, 1)
 
 ctx.fill()
 
-pyqrgen.generate(ctx)
+pyqrgen.generate("green pride", 0, 4, 2, ctx, SIZE)
+
 
 surface.write_to_png("example.png")
