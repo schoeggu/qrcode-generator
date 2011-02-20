f465f5921f96105ce4bf24d5995d5248a3f9cfcb
Joel Sutter <joel-sutter@bluewin.ch>
2011-02-20 12:58:58 +0100

    more testing with python stuff

--- src/pyqrgen/window.py
+++ src/pyqrgen/window.py
@@ -9,7 +9,7 @@ import gtk, gobject, cairo
 # Create a GTK+ widget on which we will draw using Cairo
 class Screen(gtk.DrawingArea):
 
-	surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, 200, 200)
+	surfaceSize = 3000
 
 	__gsignals__ = { "expose-event": "override" }
 
@@ -20,24 +20,76 @@ class Screen(gtk.DrawingArea):
 		cr.rectangle(event.area.x, event.area.y, event.area.width, event.area.height)
 		cr.clip()
 
-		cr.set_source_surface(self.surface, 0, 0)
+		scaleW = (event.area.width-20.0)/self.surfaceSize
+		scaleH = (event.area.height-20.0)/self.surfaceSize
+
+		cr.scale(scaleW, scaleH)
+
+		cr.set_source_surface(self.surface, 10/scaleW, 10/scaleH)
 		cr.paint()
+
+	def generate(self, data):
+		ctx = cairo.Context(self.surface)
+		pyqrgen.generate(data, 0, 4, 2, ctx, self.surfaceSize)
+		self.queue_draw()
+		
 
 	def __init__(self):
 		gtk.DrawingArea.__init__(self)
-		ctx = cairo.Context(self.surface)
-		pyqrgen.generate("green pride", 0, 4, 2, ctx, 200)
+		self.surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, self.surfaceSize, self.surfaceSize)
+		self.generate("green pride")
+
+
+
+class Win:
+
+	def clicked(self, widget):
+		self.screen.generate(self.entry.get_text())
+
+	def __init__(self):
+		self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+
+		self.window.set_title("qrgen")
+		self.window.connect("delete_event", gtk.main_quit)
+
+		self.box1 = gtk.HBox(False, 0)
+		self.box2 = gtk.VBox(False, 0)
+
+		self.window.add(self.box2)
+		self.box2.pack_start(self.box1, False, False, 0)
+
+		self.entry = gtk.Entry()
+		self.entry.set_text("green pride")
+
+		self.screen = Screen()
+		self.box2.add(self.screen)
+
+		self.button1 = gtk.Button("Generate")
+		self.button1.connect("clicked", self.clicked)
+
+		self.box1.pack_start(self.button1, True, True, 0)
+		self.box1.pack_end(self.entry, True, True, 0)
+
+		self.window.set_default_size(200, 220)
+
+		self.button1.show()
+		self.entry.show()
+
+		self.screen.show()
+		self.box1.show()
+		self.box2.show()
+		self.window.show()
+
 
 
 
 def run(Widget):
-	window = gtk.Window(gtk.WINDOW_TOPLEVEL)
-	window.connect("delete-event", gtk.main_quit)
+#	window = Widget
 	widget = Widget()
-	widget.show()
-	window.add(widget)
-	window.present()
+#	widget.show()
+#	window.add(widget)
+#	window.present()
 	gtk.main()
 
 if __name__ == "__main__":
-	run(Screen)
+	run(Win)
